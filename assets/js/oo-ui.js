9bb6ab34787b81ca77f3eac727f56d1769270834
dudung <dudung@gmail.com>
2020-10-10 19:38:24 +0700

    cotinue oo-ui

--- assets/js/oo-ui.js
+++ assets/js/oo-ui.js
@@ -7,15 +7,32 @@
 	20201010
 	1827 Start this file after confused with right directory.
 	1838 It must be included first before called.
+	1936 Test displaySimpleUI and it works.
 */
 
 
-function displayDiv() {
+// An UI
+
+
+
+// Display button than can show message when it is clicked
+function displaySimpleUI() {
+	var msg = arguments[0];
 	var div = document.createElement("div");
 	div.style.border = "1px solid #fcc";
 	div.style.background = "#fffafa";
-	div.style.height = "20px";
+	
+	var btn = document.createElement("button");
+	btn.innerHTML = "Click me!";
+	var span = document.createElement("span");
+	
 	document.body.append(div);
+	div.append(btn);
+	div.append(span);
+	
+	btn.addEventListener("click", function() {
+		span.innerHTML = "      " + msg;
+	});
 }
 
 
