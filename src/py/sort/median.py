72eb1e0ee62679522d7d1ecae531ba8769396a9c
dudung <dudung@gmail.com>
2020-11-01 05:44:25 +0700

    fin med

--- src/py/sort/median.py
+++ src/py/sort/median.py
@@ -9,6 +9,9 @@
 # 20201101
 # 0329 Start this example.
 # 0433 Use sort to find min, med, max.
+# 0530 Able to sort it manually.
+# 0538 Can find med with statistics module.
+# 0543 Finish comments.
 # 
 # References
 # 1. Vivi Andasari, "Some Basic Operations in Python", Zaman
@@ -21,21 +24,64 @@
 # 3. -, "Python List sort()", Programiz, Parewa Labs, url
 #    https://www.programiz.com/python-programming/methods/list
 #    /sort [20201101].
+# 4. Upendra Bartwal, "How to print without newline in
+#    Python?", GeeksforGeeks, Noida, India, 2 Feb 2018, url
+#    https://www.geeksforgeeks.org/print-without-newline
+#    -python/ [20201101].
+# 5. eyquem, pauloue, "Answer to 'Is there a standardized
+#    method to swap two variables in Python?'", StackOverflow,
+#    12 Feb 2013, 22 Jan 2018, url https://stackoverflow.com/
+#    a/14836456 [20201101].
+# 6. Abhijit, Banana, "Answer to 'Skip first entry in for loop
+#    in python?'", StackOverflow, 9 Apr 2012, 28 Jul 2014, url
+#    https://stackoverflow.com/a/10079247 [20201101].
+# 7. James Gallagher, "Python Median() – A Python Statistics
+#    Tutorial", Career Karma, 23 Apr 2020, url https:/
+#    /careerkarma.com/blog/python-median [20201101].
 
-# Create an array and find its min, med, max
+# Create an array and find its min, max, med using statistics
+z = [9, 1, 9, 2, 3, 3, 1, 5, 7, 9, 6, 5, 4, 4, 8, 6, 0]
+print("init =", z)
+print("min =", min(z))
+print("max =", max(z))
+import statistics
+print("mid =", statistics.median(z))
+
+# Add blank line
+print()
+
+# Create an array, sort, find its min, max, med (manually)
 x = [9, 1, 9, 2, 3, 3, 1, 5, 7, 9, 6, 5, 4, 4, 8, 6, 0]
 print("init =", x)
 x.sort()
 print("sort =", x)
 N = len(x)
 print("N =", N)
-im = int((N + 1) / 2 - 1)
-xm = x[im]
-print("min =", x[0])
-print("med =", xm)
-print("max =", x[N - 1])
+xmin = min(x)
+xmax = max(x)
+imed = int((N + 1) / 2 - 1)
+xmed = x[imed]
+print("min =", xmin)
+print("max =", xmax)
+print("med =", xmed)
 
-# Create similar array and sort it manually
+# Add blank line
+print()
+
+# Create an array, sort, find its min, max, med (all manually)
 y = [9, 1, 9, 2, 3, 3, 1, 5, 7, 9, 6, 5, 4, 4, 8, 6, 0]
+#y = [1, 8, 4, 5, 2, 1, 2]
 print("init =", y)
-
+N = len(y)
+print("N =", N)
+for i in range(0, N - 1):
+	for j in range(i + 1, N):
+		if y[i] > y[j]:
+			y[i], y[j] = y[j], y[i]
+print("sort =", y)
+imin = 0
+imax = N - 1
+imed = int((N + 1) / 2 - 1)
+print("min =", y[imin])
+print("max =", y[imax])
+print("med =", y[imed])
