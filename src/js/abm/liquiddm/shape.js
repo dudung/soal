35261d238a643fa5600f628840cae72c249d9fe5
dudung <dudung@gmail.com>
2020-12-07 20:02:13 +0700

    create shape

---
+++ src/js/abm/liquiddm/shape.js
@@ -0,0 +1,26 @@
+/*
+	shape.js
+	Shape (2d) for drawing agent in ABM
+	
+	url https://butiran.github.io/
+	
+	Sparisoma Viridi | GS _HjSaE0AAAAJ
+	
+	20201207
+	1946 Define Shape for object containing various shapes [1].
+	1950 Add shapes for neutron and proton.
+	1955 Differ shape and form [2].
+	
+	References
+	1. url https://en.wikipedia.org/w/index.php?oldid=987519327
+	   [20201207].
+	2. url http://www.differencebetween.net/language
+	   /difference-between-shape-and-form/ [20201207].
+*/
+
+// Define shapes, a 2d object
+var Shape = {};
+
+// Shapes for liquid drop model
+Shape["neutron"] = "circle";
+Shape["proton"] = "circle";
