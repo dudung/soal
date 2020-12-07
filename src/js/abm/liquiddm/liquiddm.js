41043e51309c7fde05cb3043ea071aab2638172b
dudung <dudung@gmail.com>
2020-12-07 18:39:22 +0700

    create palette

--- src/js/abm/liquiddm/liquiddm.js
+++ src/js/abm/liquiddm/liquiddm.js
@@ -2,40 +2,13 @@
 	liquiddm.js
 	Liquid drop model with agent-based model
 	
+	url https://butiran.github.io/
+	
 	Sparisoma Viridi | GS _HjSaE0AAAAJ
 	Rizal Kurniadi | GS 5NxkKtoAAAAJ
-
+	
 	20201207
 	1629 Start to design from nothing.
 	1723 Finish creating class of Nucleon.
 */
 
-
-
-
-/*
-	Class of Nucleon
-	
-	Sparisoma Viridi | GS _HjSaE0AAAAJ
-	
-	20201207
-	1633 Start designing class of Nucleon.
-	1722 Finish class of Nucleon, only simple structure.
-*/
-class Nucleon {
-	constructor() {
-		if(arguments.length == 0) {
-			this.neutron = true;
-			console.warn("Nucleon is set to neutron");
-			this.type = "neutron";
-		} else {
-			if(arguments[0] == "proton") {
-				this.proton = true;
-				this.type = "proton";
-			} else if(arguments[0] == "neutron") {
-				this.neutron = true;
-				this.type = "proton";
-			}
-		}
-	}	
-}
---
+++ src/js/abm/liquiddm/palette.js
@@ -0,0 +1,24 @@
+/*
+	palette.js
+	Palette for agent colors in ABM
+	
+	url https://butiran.github.io/
+	
+	Sparisoma Viridi | GS _HjSaE0AAAAJ
+	
+	20201207
+	1825 Define color for neutron and proton.
+	1833 Change the name from color to palette.
+	1838 Add colors for liquid drop model.
+	
+	References
+	1. Blixt, "Answer to 'Best way to store a key=>value
+	   array in JavaScript?'", StackOverflow, url https://stackoverflow.com/a/1144728 [20201207].
+*/
+
+// Define palette of colors
+var Palette = {};
+
+// Colors for liquid drop model
+Palette["neutron"] = "#ccc";
+Palette["proton"] = "#fcc";
