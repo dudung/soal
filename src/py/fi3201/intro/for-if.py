adce4a80753aa887a680f5ad19c0d2910618c5b8
dudung <dudung@gmail.com>
2021-01-22 12:04:19 +0700

    for++, codes

---
+++ src/py/fi3201/intro/for-if.py
@@ -0,0 +1,14 @@
+# 
+# for-if.py
+# Use for and if to display something in Python 3.7
+# 
+# Sparisoma Viridi | https://github.com/dudung
+# 
+# Execute: python3.7 for-if.py
+# 
+# 20210122
+# 1203 Create this example form for only.
+# 
+
+for i in range(1, 10):
+	print(i)
