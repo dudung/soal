c1a8fbbcab5ff03acc1ce1987ae6f7ee5984f8d8
dudung <dudung@gmail.com>
2021-01-30 16:37:05 +0700

    cpp

--- src/cpp/fi3201/root/root-bisection.cpp
+++ src/cpp/fi3201/root/root-bisection.cpp
@@ -10,7 +10,9 @@
 	20210130
 	1213 Start from root-scanning.cpp, root-bisection.js codes.
 	1233 Continue to modify, lunch.
-	1313 After lunch.
+	1313 After lunch, use vector, and ok.
+	1617 Continue to use SHOW_PROGRESS.
+	1636 Test for all four conditions, ok.
 */
 
 // Include necessary libraries
@@ -57,7 +59,20 @@ int main(int argc, char *argv[]) {
 			swap(x[n+1], x[n]);
 		}
 		
-		cout << n << "\t" << x[n+2] << "\t" << fn2 << endl;
+		// Display the progress
+		if(SHOW_PROGRESS) {
+			if(n == 0) {
+				double fn = test_function(x[n]);
+				cout << "n\tx\tf(x)" << endl;
+				cout << (n) << "\t";
+				cout << x[n] << "\t";
+				cout << fn << endl;
+				cout << (n+1) << "\t";
+				cout << x[n+1] << "\t";
+				cout << fn1 << endl;
+			}
+			cout << (n+2) << "\t" << x[n+2] << "\t" << fn2 << endl;
+		}
 		
 		// Caculate absoulte value of the function
 		froot = fabs(test_function(x[n+2]));
@@ -69,7 +84,11 @@ int main(int argc, char *argv[]) {
 		// Increase n
 		n++;
 	}
-	Nstep = n;
+	Nstep = n + 2;
+	
+	if(SHOW_PROGRESS) {
+		cout << endl;
+	}
 	
 	// Display result
 	cout << "f(x)  ";
@@ -84,8 +103,6 @@ int main(int argc, char *argv[]) {
 		cout << "xroot " << xroot << endl;
 	}
 	
-	cout << x.size() << endl;
-	
 	return 0;
 }
 
@@ -99,88 +116,3 @@ double test_function(double x) {
 	double y = y3 + y2 + y1 + y0;
 	return y;
 }
-
-
-/*
-// Define main function
-function main() {
-	// Define input parameters
-	var xbeg = 1;
-	var xend = 8;
-	var eps = 1E-6;
-	var Nstep = 0;
-	var SHOW_PROGRESS = false;
-	
-	// Create variables for output
-	var f = test_function;
-	var xroot = xend;
-	var maxstep = 40;
-	var n = 0;
-	
-	// Initialize variables
-	var x = [];
-	x.push(xbeg);
-	x.push(xend);
-	var froot = Math.abs(f(x[n+1]));
-	
-	// Do iteration using bisection method
-	while((froot > eps) && (n < maxstep - 1)) {
-		// Do bisection the search range
-		x[n+2] = 0.5 * (x[n+1] + x[n]);
-		
-		// Swap if necessary
-		fn1 = f(x[n+1]);
-		fn2 = f(x[n+2]);
-		c = fn2 * fn1;
-		if(c > 0) {
-			[x[n+1], x[n]] = [x[n], x[n+1]];
-		}
-		
-		// Caculate absoulte value of the function
-		froot = Math.abs(f(x[n+2]));
-		
-		// Display the progress
-		if(SHOW_PROGRESS) {
-			if(n == 1) {
-				console.log("n\tx\tf(x)");
-				var frootn = f(x[n]);
-				var frootn1 = f(x[n+1]);
-				console.log(n + "\t" + x[n] + "\t" + frootn);
-				console.log((n+1) + "\t" + x[n+1] + "\t" + frootn1);
-			}
-			var frootn2 = f(x[n+2]);
-			console.log((n+2) + "\t" + x[n+2] + "\t" + frootn2);
-		}
-		
-		// Get the xroot
-		if(froot <= eps) {
-			Nstep = n + 2;
-			xroot = x[n+2];
-		}
-		
-		// Increase n
-		n++;
-	}
-	if(SHOW_PROGRESS) {
-		console.log("");
-	}
-	
-	// Case of root not found in the search range
-	if(Nstep == 0) {
-		xroot = "not found";
-		Nstep = maxstep;
-	}	
-	
-	// Display output
-	console.log("f(x)  0.025x^3 - 0.2585x^2 + 0.243x + 0.5265");
-	console.log("xbeg  " + xbeg);
-	console.log("xend  " + xend);
-	console.log("ε     " + eps);
-	console.log("Nstep " + Nstep);
-	console.log("xroot " + xroot);
-}
-
-
-// Call main function
-main();
-*/
